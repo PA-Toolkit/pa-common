f598cd5216aec7d7f5ba78462499bf7c26761ac1
Reimnop <56994757+Reimnop@users.noreply.github.com>
2022-05-05 11:10:39 +0700

    Delete build directory

--- build/types/Utils.d.ts
+++
@@ -1,8 +0,0 @@
-export declare module Utils {
-    function randInt(max: number): number;
-    /**
-     * Creates a random ID.
-     * @return 16-character long ID string
-     */
-    function randId(): string;
-}
--- build/types/index.d.ts
+++
@@ -1,8 +0,0 @@
-import PAObject from "./PAObject";
-import Serializable from "./Serializable";
-import ObjectOwner from "./ObjectOwner";
-import KeyframeList from "./KeyframeList";
-import { Utils } from "./Utils";
-export * from "./Keyframe";
-export * from "./Enums";
-export { Utils, PAObject, Serializable, ObjectOwner, KeyframeList };
